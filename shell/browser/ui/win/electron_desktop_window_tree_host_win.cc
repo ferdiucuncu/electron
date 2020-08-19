50b3ffb143d40a28edd82027e70f76e33111df2c
Deepak Mohan <hop2deep@gmail.com>
2020-08-19 15:09:14 -0700

    fix: client area inset calculation when maximized

--- shell/browser/ui/win/electron_desktop_window_tree_host_win.cc
+++ shell/browser/ui/win/electron_desktop_window_tree_host_win.cc
@@ -4,7 +4,11 @@
 
 #include "shell/browser/ui/win/electron_desktop_window_tree_host_win.h"
 
+#include "base/win/windows_version.h"
 #include "ui/base/win/hwnd_metrics.h"
+#include "ui/base/win/shell.h"
+#include "ui/display/win/dpi.h"
+#include "ui/display/win/screen_win.h"
 
 namespace electron {
 
@@ -36,18 +40,34 @@ bool ElectronDesktopWindowTreeHostWin::HasNativeFrame() const {
   // Since we never use chromium's titlebar implementation, we can just say
   // that we use a native titlebar. This will disable the repaint locking when
   // DWM composition is disabled.
-  return true;
+  return !ui::win::IsAeroGlassEnabled();
+}
+
+bool ElectronDesktopWindowTreeHostWin::GetDwmFrameInsetsInPixels(
+    gfx::Insets* insets) const {
+  if (IsMaximized() && !native_window_view_->has_frame()) {
+    int caption_height =
+        display::win::ScreenWin::GetSystemMetricsInDIP(SM_CYSIZEFRAME) +
+        display::win::ScreenWin::GetSystemMetricsInDIP(SM_CYCAPTION);
+
+    *insets = gfx::Insets(caption_height, 0, 0, 0);
+    // The DWM API's expect values in pixels. We need to convert from DIP to
+    // pixels here.
+    *insets = insets->Scale(display::win::GetDPIScale());
+    return true;
+  }
+  return false;
 }
 
 bool ElectronDesktopWindowTreeHostWin::GetClientAreaInsets(
     gfx::Insets* insets,
     HMONITOR monitor) const {
   if (IsMaximized() && !native_window_view_->has_frame()) {
-    // Windows automatically adds a standard width border to all sides when a
-    // window is maximized.
-    int frame_thickness = ui::GetFrameThickness(monitor) - 1;
-    *insets = gfx::Insets(frame_thickness, frame_thickness, frame_thickness,
-                          frame_thickness);
+    // Reduce the Windows non-client border size because we extend the border
+    // into our client area in UpdateDWMFrame(). The top inset must be 0 or
+    // else windows will draw a full native titlebar outside the client area.
+    int frame_thickness = ui::GetFrameThickness(monitor);
+    *insets = gfx::Insets(0, frame_thickness, frame_thickness, frame_thickness);
     return true;
   }
   return false;
